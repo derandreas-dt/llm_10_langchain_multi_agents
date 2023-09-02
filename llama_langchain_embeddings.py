212c1a1c84ef503ff7d3722dff9d296f9d083c89
Andreas <derandreas.dt@gmail.com>
2023-09-02 16:42:09 +0200

    feat: initial work with HuggingFaceBgeEmbeddings and BAAI/base sentence transformer model

--- llama_langchain_embeddings.py
+++ llama_langchain_embeddings.py
@@ -1,8 +1,13 @@
-""" This is the first embedding script, that loads simple text documents
-into a vector store and enhances the LLM model with new informations.
-To run this, chromadb needs to be installed.
+""" This version uses the HuggingFaceBgeEmbeddings with the BAAI/bge sentence transformer.
+With this the embeddings are better interpreted and stored (using ChromaDB again).
 
-This uses standard llama embeddings and chromadb (pip install chromadb)
+The results could improve by using this as the LLM can work with better
+vectorized data in the embedding store.
+
+To make the HuggingFaceBgeEmbeddings work, you need to install
+
+`pip install sentence-transformers`
+
 For testing this, the plain LLM is asked for the CEO of Twitter.
 In the next example, the RetrievalQA is used, with two additional texts:
     1. Elon Musk is new CEO
@@ -10,22 +15,48 @@ In the next example, the RetrievalQA is used, with two additional texts:
 
 One text is english and one is german.
 
-(Note the texts are in order of the date, if not it is wrong!)
-
 The reults are:
 
 ```
 Anwer without langchain embeddings to the question: Who is CEO of twitter?
-As of March 2023, Twitter's CEO is Jack Dorsey. He co-founded Twitter and has been the CEO twice, first from 2006 to 2008 and again since 2015.
-Context: You are looking for information about the CEO of Twitter.
+First, we need to determine who the current CEO of Twitter is. To do this, we can
+check Twitter's official website or social media accounts for an update on their
+leadership team. As of my knowledge cutoff in December 2022, the CEO of Twitter is
+Jack Dorsey. However, it's important to note that Twitter's leadership may change
+over time, so this information could be outdated.
+Context: The user is asking about the current CEO of Twitter, and you are providing
+an answer based on your knowledge cutoff in December 2022.
 
 Answer with Langchain embeddings to the question Who is CEO of twitter?
-On 01.07.2023, it was announced that Linda Yaccarino would be the new CEO of Twitter. So, the answer is Linda Yaccarino.
+On 01.07.2023, Elon Musk announced that Linda Yaccarino would be the new CEO of Twitter.
+So, the answer to your question is Linda Yaccarino.
 ```
+
+With the normal LLamaCppEmbeddings, the order of the "news texts" were important.
+This time, the order is swapped, so the announcement of Musk is the second text,
+which could be identified as "newer". So swapping this, could not be better
+
+Results (swapping text_1, text_2 variable names):
+
+```
+Anwer without langchain embeddings to the question: Who is CEO of twitter?
+1. Check Twitter's official website or Twitter account for the most up-to-date information on the CEO.
+2. Search online for recent news articles or press releases about Twitter's leadership team.
+3. If you are still unsure, try reaching out to Twitter's customer support team for more information.
+
+Unsure about answer. Can you provide more context or clarify the question?
+
+Answer with Langchain embeddings to the question Who is CEO of twitter?
+As of 01.10.2022, Elon Musk was the new CEO of Twitter. However, as of 01.07.2023, Linda Yaccarino became the new CEO of Twitter. So, the answer is Linda Yaccarino.
+Context: The given text is about Elon Musk becoming the new CEO of Twitter and then Linda Yaccarino replacing him as the new CEO.
+```
+
+Great!
+
 """
 # llamacpp and embeddings
 from langchain.llms import LlamaCpp
-from langchain.embeddings import LlamaCppEmbeddings
+from langchain.embeddings import HuggingFaceBgeEmbeddings
 
 # text loaded and processor
 from langchain.document_loaders import TextLoader
@@ -42,6 +73,7 @@ from langchain import PromptTemplate, LLMChain
 
 # PATH where the model is on your device
 MODEL_PATH = "/home/andreas/development/ai/models/llama-2-7b-chat.ggmlv3.q4_0.bin"
+MODEL_NAME_EMBEDDINGS = "BAAI/bge-base-en"
 MODEL_PARAMS = {
     "model_path": MODEL_PATH,
     "n_ctx": 2048, # context size
@@ -67,8 +99,13 @@ prompt = PromptTemplate(
 # create an llm chain to have the primpt
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-# init the llamacpp embeddings
-llama_embeddings = LlamaCppEmbeddings(**MODEL_PARAMS)
+# init the HuggingFaceBgeEmbeddings
+hf_embeddings = HuggingFaceBgeEmbeddings(
+    model_name=MODEL_NAME_EMBEDDINGS,
+    encode_kwargs={
+        'normalize_embeddings': True, # set True to compute cosine similarity
+    }
+)
 
 ## add some simple text embeddings inline
 text_1 = """01.10.2022 Elon Musk is new CEO of Twitter"""
@@ -88,7 +125,7 @@ chromadb = Chroma.from_texts(
         text_1,
         text_2
     ],
-    llama_embeddings,
+    hf_embeddings,
     metadatas=[
         {"source": "my-own-source-text-1"},
         {"source": "my-own-source-text-2"}
